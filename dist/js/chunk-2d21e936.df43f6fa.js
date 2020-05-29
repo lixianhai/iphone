0b9aed08386152b35d10d00aef426146fa404ec8
lixianhai <lixianhai@yinhechuangxiang.com>
2020-05-29 17:47:41 +0800

    vue.config

--- dist/js/chunk-2d21e936.df43f6fa.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21e936"],{d5c2:function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[e._v("register")])},r=[],u=t("2877"),l={},s=Object(u["a"])(l,c,r,!1,null,null,null);n["default"]=s.exports}}]);
-//# sourceMappingURL=chunk-2d21e936.df43f6fa.js.map
--- vue.config.js
+++ vue.config.js
@@ -9,4 +9,14 @@ module.exports = {
   publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
+  productionSourceMap: false,
+  devServer: {
+    port: 9527,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    },
+    proxy: {}
+  }
 }
