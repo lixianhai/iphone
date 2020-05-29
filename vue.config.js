a96a91107fae35b0d470038ed485487484722cfc
lixianhai <lixianhai@yinhechuangxiang.com>
2020-05-29 17:49:08 +0800

    vue.config

--- vue.config.js
+++ vue.config.js
@@ -9,14 +9,7 @@ module.exports = {
   publicPath: './',
   outputDir: 'dist',
   assetsDir: 'static',
-  productionSourceMap: false,
   devServer: {
-    port: 9527,
-    open: true,
-    overlay: {
-      warnings: false,
-      errors: true
-    },
-    proxy: {}
+    port: 9527
   }
 }
